4505aca308e31838cbadfd4d42dd641a9e5b125c
Gosols <teemu.a.kosonen@hotmail.com>
2020-02-12 19:12:22 +0200

    jonkinlainen toimivuus refaktoroinnin jälkeen

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,6 +1,21 @@
 import React from "react";
 
-export default function FormComp(props) {
+export default function TodoForm(props) {
+  const [input, setInput] = React.useState({ desc: "", date: "" });
+  
+  const addTodo = (event) => {
+    event.preventDefault();
+
+    setInput({ desc: "", date: "" });
+    props.appCallback(input);
+    
+  };
+
+  const inputChanged = event => {
+    setInput({ ...input, [event.target.name]: event.target.value });
+  };
+
+
   return (
     <div>
       <div className="inputfield">
@@ -9,19 +24,19 @@ export default function FormComp(props) {
           name="desc"
           type="text"
           placeholder="Enter text"
-          value={props.input.desc}
-          onChange={props.inputChanged}
+          value={input.desc}
+          onChange={inputChanged}
         />
 
         <label> Date: </label>
         <input
           name="date"
           type="date"
-          value={props.input.date}
-          onChange={props.inputChanged}
+          value={input.date}
+          onChange={inputChanged}
         ></input>
         <label> </label>
-        <button onClick={props.addTodo}>Add</button>
+  <button onClick={addTodo}>Add</button>
       </div>
     </div>
   );
--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,5 +1,34 @@
 import React from "react";
+import ReactTable from "react-table-v6";
+import "react-table-v6/react-table.css";
 
 export default function Todolist(props) {
-  return <div></div>;
+  const [todos, setTodos] = React.useState([]);
+
+  const deleteTodo = event => {
+    setTodos(todos.filter((_, i) => i !== Number(event.target.id)));
+  };
+
+  const columns = [
+    {
+      Header: "Description",
+      accessor: "desc"
+    },
+    {
+      Header: "Date",
+      accessor: "date"
+    },
+    {
+      Cell: ({ index }) => (
+        <button id={index} onClick={deleteTodo}>
+          Delete
+        </button>
+      ),
+      filterable: false,
+      sortable: false,
+      width: 80
+    }
+  ];
+
+  return <ReactTable data={todos} columns={columns} filterable="true" />;
 }
