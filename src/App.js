22ec9e6aa326df7d3c505f7e085adc94669fb6e7
Gosols <teemu.a.kosonen@hotmail.com>
2020-02-12 12:38:54 +0200

    poistotoiminnallisuus lisätty

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from "react";
 import "./App.css";
-import Todolist from "./Todolist";
+import Todolist from "./components/Todolist";
+import FormComp from "./components/form";
 
-function App() {
+export default function App() {
+  const [input, setInput] = React.useState({ desc: "", date: "" });
+  const [todos, setTodos] = React.useState([]);
+
+  const deleteTodo = event => {
+    console.log(`Olen eventin nimi HÖHÖÖ: ${event.target.name}`);
+    setTodos(todos.filter((_, i) => i !== Number(event.target.name)));
+    console.log(todos);
+  };
+
+  const addTodo = event => {
+    event.preventDefault();
+    setTodos([...todos, input]);
+  };
+
+  const inputChanged = event => {
+    setInput({ ...input, [event.target.name]: event.target.value });
+  };
+
   return (
     <div className="App">
       <header className="App-header">TodoList</header>
-      <Todolist />
+      <FormComp inputChanged={inputChanged} input={input} addTodo={addTodo} />
+      <Todolist todos={todos} deleteTodo={deleteTodo} />
     </div>
   );
 }
-
-export default App;
